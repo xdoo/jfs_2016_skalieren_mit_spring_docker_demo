bf60b3e9616c860d50f7a1e47b87c91c21f86b57
xdoo <claus.straube@gmail.com>
2016-07-06 11:29:57 +0200

    renamed Application.java

--- demo-proxy/src/main/java/com/example/Application.java
+++ demo-proxy/src/main/java/com/example/Application.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-public class DemoProxyApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoProxyApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
