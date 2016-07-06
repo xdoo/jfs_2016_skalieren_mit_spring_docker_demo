b57d41e9da87322fbc6de228b2eb185758ada37e
xdoo <claus.straube@gmail.com>
2016-07-06 11:27:09 +0200

    renamed Application.java

--- demo-registry/src/main/java/com/example/Application.java
+++ demo-registry/src/main/java/com/example/Application.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class DemoRegistryApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoRegistryApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
