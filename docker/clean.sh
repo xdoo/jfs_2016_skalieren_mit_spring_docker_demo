a63ce0695a2973f2a17dea5a0f173f5a932803fa
xdoo <claus.straube@gmail.com>
2016-07-07 09:01:37 +0200

    added complete cluster to clean script

--- docker/clean.sh
+++ docker/clean.sh
@@ -1,11 +1,15 @@
 echo "removing containers"
 docker rm docker_service-a_1
+docker rm docker_service-a_2
 docker rm docker_service-b_1
+docker rm docker_service-b_2
+docker rm docker_service-b_3
+docker rm docker_service-b_4
 docker rm docker_service-registry_1
 docker rm docker_zuul-proxy_1
 
 echo "removing images"
 docker rmi docker_service-a
 docker rmi docker_service-b
-docker rmi docker_service-service-registry
-docker rmi docker_service-zuul-proxy
+docker rmi docker_service-registry
+docker rmi docker_zuul-proxy
